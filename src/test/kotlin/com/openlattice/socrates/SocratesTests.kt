fcfb1682708745664ca9717297205d1e912f950d
Drew Bailey <drew@openlattice.com>
2020-12-21 17:14:33 -0500

    add a fake tests

---
+++ src/test/kotlin/com/openlattice/socrates/SocratesTests.kt
@@ -0,0 +1,14 @@
+package com.openlattice.socrates
+
+import org.junit.Test
+
+/**
+ * @author Drew Bailey (drew@openlattice.com)
+ */
+class SocratesTests {
+
+    @Test
+    public fun passme() {
+        assert(true)
+    }
+}
