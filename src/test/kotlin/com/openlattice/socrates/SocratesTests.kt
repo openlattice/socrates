49367915f6b116f89e23c1766502585a081c940b
Drew Bailey <drew@openlattice.com>
2020-12-21 17:25:57 -0500

    fix spotbugs

--- src/test/kotlin/com/openlattice/socrates/SocratesTests.kt
+++ src/test/kotlin/com/openlattice/socrates/SocratesTests.kt
@@ -1,14 +1,16 @@
 package com.openlattice.socrates
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.junit.Test
 
 /**
  * @author Drew Bailey (drew@openlattice.com)
  */
+@SuppressFBWarnings("UCF_USELESS_CONTROL_FLOW_NEXT_LINE")
 class SocratesTests {
 
     @Test
-    public fun passme() {
+    fun pass() {
         assert(true)
     }
 }
